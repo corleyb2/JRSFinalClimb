837a5e4ef5c33a5f1f92569ae380fd374c328853
corleyb2 <corleyb2@gmail.com>
2020-06-14 16:49:04 -0400

    remove getUserProfile Redux files, confirmed hard-coded axios call works from component level

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,21 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 //will transition to EditProfile page onclick
 
-const UserProfile = ({ boundGetUserProfile, profile, avatar }) => {
-  console.log("profile", profile);
+const UserProfile = () => {
+  const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
-    boundGetUserProfile();
+    async function getUserProfile() {
+      //establish username here - using auth?  temporary hard code.
+      const username = "test1";
+      const avatarResponse = null;
+      const response = await axios({
+        method: "GET",
+        url: `http://localhost:4000/user`,
+        data: {
+          username: username,
+        },
+        header: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => {
+        const profileResponse = response.data;
+        console.log("finding user.2", profileResponse.username);
+        setCurrentUser(profileResponse);
+        // navigate("/user");
+      });
+      // if (response.data[0][0] == undefined) {
+      //   let response = undefined;
+      //   let avatar = undefined;
+      //   dispatch(getUserProfileSuccess(response, avatar));
+      // } else {
+      //   console.log(response.data[0][0]);
+      //   const uuid = response.data[0][0].avatar;
+      //   const avatar = await Storage.get("profilepics/" + uuid, {
+      //     // level: "protected",
+      //     contentType: "image/png",
+      //   });
+      //   const profileResp = response.data[0][0];
+    }
+    getUserProfile();
   }, []);
 
   return (
     <div>
       <h2>View User Profile</h2>
-      Type in Username:
-      <p>{profile.username}</p>
-      <p>{profile.firstname}</p>
-      <p>{profile.lastname}</p>
+      <p>{currentUser.username}</p>
+      <p>{currentUser.firstname}</p>
+      <p>{currentUser.lastname}</p>
     </div>
   );
 };
--- src/reducers/combineReducers.js
+++ src/reducers/combineReducers.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 
 import { createUserProfileReducer } from "./createUserProfileReducer";
-import { getUserProfileReducer } from "./getUserProfileReducer";
 
 const totalReducer = combineReducers({
   createUserProfileReducer,
-  getUserProfileReducer,
 });
 
 export const rootReducer = (state, action) => {
