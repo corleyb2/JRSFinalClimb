7823c5f313d44378b8455dee8180dd68983d524b
corleyb2 <corleyb2@gmail.com>
2020-06-20 20:06:17 -0400

    RelationalModel updated to reference climb & user tables

--- server/models/relationalModel.js
+++ server/models/relationalModel.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 const relationalSchema = new mongoose.Schema({
   scheduledTrip: {
-    type: String,
-    // type: mongoose.Schema.Types.ObjectId,
-    // required: true,
-    // ref: "climb",
+    // type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "climb",
   },
   scheduledUsers: {
-    type: [String],
-    // type: [mongoose.Schema.Types.ObjectId],
-    // required: true,
-    // ref: "user",
+    // type: [String],
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: "user",
   },
 });
 
